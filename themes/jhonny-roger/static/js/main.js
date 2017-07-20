178b197914f24d96f5c66220913f36f975084f20
Jhonny Roger Silva <jhonnyrogersilva@gmail.com>
2017-07-20 00:21:39 -0300

    foi finalizado a logica de busca

--- themes/jhonny-roger/static/js/main.js
+++ themes/jhonny-roger/static/js/main.js
@@ -3,7 +3,17 @@ $(document).ready(function(){
 	var header = $('#sd-header');
 	var htmlHeight = $("#sd-main-post").height();
 
+	$('body').on('click', function(){
+		$("#search-results ul").html("")
+	})
+
 	$(window).scroll(function () {
+		if($("#search-results ul li").length == 0){
+			header.css("position", "fixed")
+		}else{
+			header.css("position", "relative")
+		}
+
 		if(header.css("position") === "fixed"){
 
 			var iCurScrollPos = $(this).scrollTop();
@@ -22,7 +32,6 @@ $(document).ready(function(){
 				}
 			}
 
-
 			if(iCurScrollPos > iScrollPos && iCurScrollPos > htmlHeight - 200){
 				header.slideUp(200)
 			}else{
@@ -32,4 +41,30 @@ $(document).ready(function(){
 			iScrollPos = iCurScrollPos;
 		}
 	});
-});
+	
+	$.getJSON('/index.json', function(data2){
+		$.getJSON('/search-index.json', function(data){
+			var idx = lunr.Index.load(data);
+
+			console.log(idx)
+
+			$("#search-input").on('keyup keydown', function(){
+				var response = idx.search($("#search-input").val().toLowerCase())
+				$("#search-results ul").html("")
+
+				if(!header.hasClass("inverted")){
+					header.addClass("inverted")
+					$('#sd-logo').addClass("inverted")
+					$('#sd-nav-menu').addClass("inverted")
+				}
+				
+				response.forEach(function(item, index){
+					var post = data2[item.ref]
+					$("#search-results ul").append("<li><a href='" + post.ref + "'>" + post.title + "</a></li>")
+				})
+			})
+		})
+	})
+})
+
+
