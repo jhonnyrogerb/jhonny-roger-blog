37f2c1ddd565c17de06f6e763cd85aba8406b417
Jhonny Roger Silva <jhonnyrogersilva@gmail.com>
2017-07-28 23:08:50 -0300

    novo teste

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,4 +4,4 @@ set -e
 
 sudo apt-get install -y rsync
 
-rsync -e "ssh -o StrictHostKeyChecking=no" -e "ssh -i /tmp/id_rsa" -ravzup public/* jhonnyroger@jhonnyroger.com:~/public_html/
+rsync -e "ssh -o StrictHostKeyChecking=no -i /tmp/id_rsa" -ravzup public/* jhonnyroger@jhonnyroger.com:~/public_html/
